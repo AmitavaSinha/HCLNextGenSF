dfa040379de208902f9bb70fccdbd57eb9e6e2bf
Amitava Sinha <amitava.sinha@gmail.com>
2018-12-14 11:20:58 +0000

    Initial Commit

---
+++ Order.Test/OrderTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ServiceFabric.Mocks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Order.Test
+{
+    public class OrderTest
+    {
+        [TestFixture]
+        public class VotingTest
+        {
+            [Test]
+            public async Task PutTest()
+            {
+
+                MockReliableStateManager stateManager = new MockReliableStateManager();
+                OrderData.Controllers.VoteDataController target = new OrderData.Controllers.VoteDataController(stateManager);
+                //var actual = await target.Get();
+                IActionResult result = await target.Put("Cricket");
+                var okResult = result as OkResult;
+                //Assert.IsNotNull(okResult);
+                Assert.AreEqual(200, okResult.StatusCode);
+                Assert.AreEqual("2", "2");
+            }
+        }
+    }
+}
